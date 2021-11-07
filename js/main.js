72020c87ed678221cbf34c6606bbbf1789bcb2c7
devchoplife <dritedurojaiye@gmail.com>
2021-11-07 09:39:55 +0100

    Draw horizontal column chart with series

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Simple Horizontal Bar Chart
 JSC.Chart("chart1", {
     type: 'horizontal column',
     series: [{
@@ -7,3 +8,23 @@ JSC.Chart("chart1", {
         ]
     }]
 });
+
+//Horizontal Bar Chart wit multiple charts
+JSC.Chart("chart2", {
+    type: "horizontal column",
+    series: [{
+        name: "Andy",
+        points: [
+            { x: "Apples", y: 50 },
+            { x: "Oranges", y: 32 }
+        ]
+    }, {
+        name: "Anna",
+        points: [
+            { x: "Apples", y: 30 },
+            { x: "Oranges", y: 22 }
+        ]
+    }]
+})
+
+//Ge csv file for the tutorial here - https://data.cdc.gov/resource/w9j2-ggv5.csv
