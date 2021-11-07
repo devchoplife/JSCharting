e64c7bfd81f1930c63f67718bd39e9764b643201
devchoplife <dritedurojaiye@gmail.com>
2021-11-07 10:33:43 +0100

    Draw chart from a CSV file

--- js/main.js
+++ js/main.js
@@ -28,3 +28,51 @@ JSC.Chart("chart2", {
 })
 
 //Ge csv file for the tutorial here - https://data.cdc.gov/resource/w9j2-ggv5.csv
+
+//fetch the csv file 
+fetch('https://data.cdc.gov/resource/w9j2-ggv5.csv')
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(text) {
+        let series = csvToSeries(text);
+        renderChart(series);
+    })
+    .catch(function(error) {
+        //throw error if csv file cannot be downloaded
+        console.log(error)
+    });
+
+
+
+function csvToSeries(text) {
+    const lifeExp = "average_life_expectancy";
+    let male = [],
+        female = [];
+    //convert the csv text to json
+    let jsonData = JSC.csv2Json(text);
+    //loop over the json data 
+    jsonData.forEach(function(row) {
+        //Add either to male or female or discard
+        if (row.race === 'All Races') {
+            if (row.sex === 'Male') {
+                male.push({ x: row.year, y: row[lifeExp] });
+            } else if (row.sex === 'Female') {
+                female.push({ x: row.year, y: row[lifeExp] })
+            }
+        }
+    })
+
+    console.log([male, female])
+    return [
+        { name: 'male', points: male },
+        { name: 'Female', points: female }
+    ];
+}
+
+function renderChart(series) {
+    JSC.Chart('chart3', {
+
+        series: series
+    })
+}
